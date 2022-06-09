bdd71f806b0a1a51140875594fd812db53482885
ToniNichev <toni.nichev@gmail.com>
2022-06-09 11:50:18 -0400

    Adding array support

--- index.js
+++ index.js
@@ -3,44 +3,54 @@ var { graphqlHTTP } = require('express-graphql');
 var graphql = require('graphql');
 
 // Maps id to User object
-var fakeDatabase = {
-  'a': {
-    id: 'a',
+var fakeDatabase = [
+  {
+    id: 1,
     name: 'alice',
   },
-  'b': {
-    id: 'b',
+  {
+    id: 2,
     name: 'bob',
   },
-};
+  {
+    id: 3,
+    name: 'john',
+  },
+];
 
 // Define the User type
 var userType = new graphql.GraphQLObjectType({
   name: 'User',
   fields: {
-    id: { type: graphql.GraphQLString },
+    id: { type: graphql.GraphQLInt },
     name: { type: graphql.GraphQLString },
   }
 });
 
 // Define the Query type
-var queryType = new graphql.GraphQLObjectType({
+var RootQuery = new graphql.GraphQLObjectType({
   name: 'Query',
   fields: {
-    user: {
+    getUser: {
       type: userType,
       // `args` describes the arguments that the `user` query accepts
       args: {
-        id: { type: graphql.GraphQLString }
+        id: { type: graphql.GraphQLInt }
       },
       resolve: (_, {id}) => {
         return fakeDatabase[id];
       }
+    },
+    getAllUsers: {
+        type: new graphql.GraphQLList(userType),
+        resolve: () => {
+            return fakeDatabase;
+        }
     }
   }
 });
 
-var schema = new graphql.GraphQLSchema({query: queryType});
+var schema = new graphql.GraphQLSchema({query: RootQuery});
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
