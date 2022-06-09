2d1bfcb141a0fa50f65aa9d23e0304ae6130b2cd
ToniNichev <toni.nichev@gmail.com>
2022-06-09 15:01:24 -0400

    #Stable, added different types

--- index.js
+++ index.js
@@ -4,58 +4,83 @@ var graphql = require('graphql');
 
 // Maps id to User object
 var fakeDatabase = [
-  {
-    id: 1,
-    name: 'alice',
-  },
-  {
-    id: 2,
-    name: 'bob',
-  },
-  {
-    id: 3,
-    name: 'john',
-  },
+    {
+        id: 1,
+        name: 'alice',
+    },
+    {
+        id: 2,
+        name: 'bob',
+    },
+    {
+        id: 3,
+        name: 'john',
+    },
 ];
 
 // Define the User type
-var userType = new graphql.GraphQLObjectType({
-  name: 'User',
-  fields: {
-    id: { type: graphql.GraphQLInt },
-    name: { type: graphql.GraphQLString },
-  }
+const userType = new graphql.GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: { type: graphql.GraphQLInt },
+        name: { type: graphql.GraphQLString },
+    }
+});
+
+const allUsersType = new graphql.GraphQLObjectType({
+    name: 'AllUsersType',
+    fields: {
+        count: { type: graphql.GraphQLInt },
+        label: { type: graphql.GraphQLString },
+        allUsersList: { type: new graphql.GraphQLList(userType) },
+    }
 });
 
 // Define the Query type
-var RootQuery = new graphql.GraphQLObjectType({
-  name: 'Query',
-  fields: {
-    getUser: {
-      type: userType,
-      // `args` describes the arguments that the `user` query accepts
-      args: {
-        id: { type: graphql.GraphQLInt }
-      },
-      resolve: (_, {id}) => {
-        return fakeDatabase[id];
-      }
-    },
-    getAllUsers: {
-        type: new graphql.GraphQLList(userType),
-        resolve: () => {
-            return fakeDatabase;
+const RootQuery = new graphql.GraphQLObjectType({
+    name: 'Query',
+    fields: {
+        // return userType
+        getUser: {
+            type: userType,
+            // `args` describes the arguments that the `user` query accepts
+            args: {
+                id: { type: graphql.GraphQLInt }
+            },
+            resolve: (_, { id }) => {
+                return fakeDatabase[id];
+            }
+        },
+        // return a list of userType (an array)
+        getAllUsers: {
+            type: new graphql.GraphQLList(userType),
+            resolve: () => {
+                return fakeDatabase;
+            }
+        },
+        // return object on allUserType
+        getAllUsersList: {
+            type: allUsersType,
+            args: {
+                label: { type: graphql.GraphQLString },
+            },
+            resolve: (label) => {
+                return {
+                    count: fakeDatabase.length,
+                    label: "123",
+                    allUsersList: fakeDatabase,
+                }
+            }
         }
     }
-  }
 });
 
-var schema = new graphql.GraphQLSchema({query: RootQuery});
+const schema = new graphql.GraphQLSchema({ query: RootQuery });
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  graphiql: true,
+    schema: schema,
+    graphiql: true,
 }));
 app.listen(4000);
 console.log('Running a GraphQL API server at localhost:4000/graphql');
